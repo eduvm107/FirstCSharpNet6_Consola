9609d4574041364211a5f9c02b8d36b84e93d810
Eduardo Vergara <eduvm107@gmail.com>
2025-05-08 20:33:54 -0500

    Add project files.

---
+++ FirstCSharpNet6/Personas.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstCSharpNet6
+{
+    public class Personas
+    {
+        public int dni { get; set; }
+        public String Nombre { get; set; }
+        public int telefono { get; set; }
+        public bool estado { get; set; }
+      
+
+        public Personas(int dni, String nombre, String tefefono, bool estado)
+        {
+            this.dni = dni;
+            this.Nombre = nombre;
+            this.telefono = telefono;
+            this.estado = estado;
+        }
+        public Personas()
+        {
+
+        }
+
+    }
+}
---
+++ FirstCSharpNet6/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+String curso = "Desarrollo de aplicacion del 2025, remontada epica";
+int edad = 22;
+double salary = 100000;
+var alumno = "Eduardo";
